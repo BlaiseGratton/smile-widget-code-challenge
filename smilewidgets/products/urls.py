ecde9029d8e44b766fe6dc173ec61c20b1b9376a
BlaiseGratton <blaisegratton@gmail.com>
2018-08-14 23:12:57 -0500

    initial setup for get-price endpoint and query params

---
+++ smilewidgets/products/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('get-price', views.get_product_price, name='get-product-price')
+]
--- smilewidgets/products/views.py
+++ smilewidgets/products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from pprint import pprint
 
-# Create your views here.
+
+def get_product_price(request):
+    params = request.GET
+
+    if 'productCode' not in params or 'date' not in params:
+        message = 'Missing "productCode" or "date" query parameters'
+        status_code = 422
+    else:
+        product_code = params.get('productCode')
+        date = params.get('date')
+        message = f'Product code is {product_code}; Date is {date}'
+        status_code = 200
+
+    return JsonResponse({'message': message}, status=status_code)
